ad1d8618c9068cff5c414efbf562d0359faf4cb5
Cassandra Comar <ccomar@drwholdings.com>
2025-08-11 18:17:50 -0400

    sigh, can't update containers

--- flake.nix
+++ flake.nix
@@ -41,12 +41,8 @@
         #     hash = "sha256-LZ10czBn5oaKMHQ8xguC6VZa7wvEgPRu6oWt/22QaDs=";
         #   };
         # });
-        hadrian = pkgs.haskell.compiler.ghc9122.hadrian.override {
-          containers = pkgs.haskell.compiler.ghc9122.bootPkgs.containers_0_8;
-        };
         ghc =
           (pkgs.haskell.compiler.ghc9122.override {
-            inherit hadrian;
             useLLVM = true;
           }).overrideAttrs (old:
             pkgs.lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
